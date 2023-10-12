cf58fb6ed7a8b64ca9abd4c5696c5924f8471807
Jeongyong Lee <hua9378@gmail.com>
2023-10-12 15:48:51 +0900

    fix: 스레드 이름을 설정하도록 수정

--- src/main/java/com/nhnacademy/aiot/node/Node.java
+++ src/main/java/com/nhnacademy/aiot/node/Node.java
@@ -1,13 +1,31 @@
 package com.nhnacademy.aiot.node;
 
+/**
+ * 스레드로 동작하는 노드 클래스입니다.
+ */
 public abstract class Node implements Runnable {
 
-    private final Thread thread = new Thread(this);
+    private final Thread thread;
 
+    /**
+     * 노드를 생성합니다.
+     * 
+     * @param name 노드의 이름입니다.
+     */
+    protected Node(String name) {
+        thread = new Thread(this, name);
+    }
+
+    /**
+     * 노드를 실행합니다.
+     */
     public void start() {
         thread.start();
     }
 
+    /**
+     * 노드를 종료합니다.
+     */
     public void stop() {
         thread.interrupt();
     }
