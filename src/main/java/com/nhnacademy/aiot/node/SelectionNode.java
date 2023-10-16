b29d3a78925c8f7749988d3dc49ecf91baf667fb
NamJiMin <njmin8@naver.com>
2023-10-16 21:35:38 +0900

    fix: SelectionNode 구현 변경

--- src/main/java/com/nhnacademy/aiot/node/SelectionNode.java
+++ src/main/java/com/nhnacademy/aiot/node/SelectionNode.java
@@ -1,11 +1,9 @@
 package com.nhnacademy.aiot.node;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-import java.util.function.Predicate;
+import java.util.function.Function;
 
 import com.nhnacademy.aiot.message.Message;
 import com.nhnacademy.aiot.pipe.Pipe;
@@ -16,29 +14,31 @@ import lombok.extern.slf4j.Slf4j;
  * 특정 message를 특정 출력 port로 내보내는 노드입니다.
  */
 @Slf4j
-public class SelectionNode<T> implements InputNode, OutputNode {
-    private static final String INVALIDPORT = "{}: 유효하지 않은 포트를 선택했습니다.{}";
+public class SelectionNode implements InputNode, OutputNode {
+    private static final String INVALID_PORT = "{}: 유효하지 않은 포트를 선택했습니다.({})";
+    private static final String NODE_STARTED = "{}: Selection Node가 시작되었습니다.";
+    private static final String NODE_STOPPED = "{}: Selection Node가 종료되었습니다.";
+    private static final String SEND_MESSAGE = "{}: {}번 파이프로 {}를 보냈습니다.";
 
     private final UUID uuid;
     private final Thread thread;
     private final Map<Integer, Pipe> outPipes;
-    private final List<Integer> ports;
+    private final Function<Message, Integer> function;
     private Pipe inPipe;
-    private Predicate<T> predicate;
 
     /**
-     * @param predicate 조건식
+     * @param predicate   조건식
      * @param outPipeNums 조건식이 true인 메세지를 보낼 포트
      */
-    public SelectionNode(Predicate<T> predicate, int... outPipeNums) {
+    public SelectionNode(Function<Message, Integer> function) {
         this.uuid = UUID.randomUUID();
-        this.thread = new Thread(this);
-        this.predicate = predicate;
+        this.thread = new Thread(this, "SelectionNode(" + getUuid() + ")");
+        this.function = function;
         outPipes = new HashMap<>();
-        ports = new ArrayList<>();
-        for (int i : outPipeNums) {
-            ports.add(i);
-        }
+    }
+
+    public UUID getUuid() {
+        return uuid;
     }
 
     @Override
@@ -51,37 +51,38 @@ public class SelectionNode<T> implements InputNode, OutputNode {
         thread.interrupt();
     }
 
-    /**
-     * 입력한 port가 outPipes에 있는지 확인하고, 조건식이 true인 메세지만 지정한 port로 내보냅니다.
-     * @throws InterruptedException 인터럽트가 발생한 경우
-     */
-    private void process() throws InterruptedException {
-        setPredicate();
-        Message message = inPipe.get();
-        if (predicate.test((T) message.get("value"))) {
-            for (int port : ports) {
-                outPipes.get(port).put(message);
-            }
-        }
-    }
-
-    private void setPredicate() {
-        for (int num : ports) {
-            if (!outPipes.containsKey(num)) {
-                log.error(INVALIDPORT, uuid, num);
-                ports.remove(num);
-            }
-        }
-    }
-
     @Override
     public void run() {
+        preprocess();
         while (!Thread.currentThread().isInterrupted()) {
-            try {
-                process();
-            } catch (InterruptedException ignore) {
-                Thread.currentThread().interrupt();
-            }
+            process();
+        }
+        postprocess();
+    }
+
+    private void preprocess() {
+        log.debug(NODE_STARTED, getUuid());
+    }
+
+    private void process() {
+        try {
+            Message message = inPipe.get();
+            sendMessage(message, function.apply(message));
+            log.debug(SEND_MESSAGE, getUuid(), function.apply(message), message.get("value"));
+        } catch (InterruptedException ignore) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private void postprocess() {
+        log.debug(NODE_STOPPED, getUuid());
+    }
+
+    private void sendMessage(Message message, int port) throws InterruptedException {
+        if (outPipes.get(port) == null) {
+            log.debug(INVALID_PORT, getUuid(), port);
+        } else {
+            outPipes.get(port).put(message);
         }
     }
 
--- src/test/java/com/nhnacademy/aiot/node/SelectionNodeTest.java
+++ src/test/java/com/nhnacademy/aiot/node/SelectionNodeTest.java
@@ -1,6 +1,6 @@
 package com.nhnacademy.aiot.node;
 
-import java.util.function.Predicate;
+import java.util.function.Function;
 
 import com.nhnacademy.aiot.message.Message;
 import com.nhnacademy.aiot.pipe.Pipe;
@@ -13,23 +13,31 @@ public class SelectionNodeTest {
         Pipe pipe2 = new Pipe();
         Pipe pipe3 = new Pipe();
 
-        Predicate<Integer> predicate = i -> i > 1;
-
-        
-        SelectionNode<Integer> SelectionNode = new SelectionNode<>(predicate, 0);
+        Function<Message, Integer> function = message -> {
+            if (message.getInt("value") < 0)
+                return 0;
+            else if (message.getInt("value") % 2 == 0) {
+                return 1;
+            } else {
+                return 123049821;
+            }
+        };
+        SelectionNode SelectionNode = new SelectionNode(function);
         SelectionNode.connectIn(0, pipe1);
         SelectionNode.connectOut(0, pipe2);
         SelectionNode.connectOut(1, pipe3);
         SelectionNode.start();
-        
+
         Message message = new Message(60_000);
         message.put("value", 10);
         Message message2 = new Message(60_000);
         message2.put("value", -5);
+        Message message3 = new Message(60_000);
+        message3.put("value", 11);
         pipe1.put(message);
         pipe1.put(message2);
+        pipe1.put(message3);
 
-        System.out.println(pipe2.get().getInt("value"));
         System.out.println(pipe2.get().getInt("value"));
         System.out.println(pipe3.get().getInt("value"));
     }
